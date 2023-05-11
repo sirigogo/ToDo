2e7b401d240ffbcaa274c3a3bdd9994971688cb4
gsjang94@gamil.com <gsjang94@gmail.com>
2023-05-11 22:56:02 +0900

    clear

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.css";
 import { useState } from "react";
+import List from "./components/List";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const changeTodo = (e) => {
+  const [toDo, setToDo] = useState([]);
+  const [toDos, setToDos] = useState([
+    { no: 3, title: "밥먹기" },
+    { no: 2, title: "공부하기" },
+    { no: 1, title: "리액트부시기" },
+  ]);
+  const plusTodo = (e) => {
     setToDo(e.target.value);
   };
   const onSubmitForm = (e) => {
     e.preventDefault();
-    setToDos((x) => [toDo, ...x]);
+    setToDos((prev) => [
+      {
+        no: toDos[0].no + 1,
+        title: toDo,
+      },
+      ...prev,
+    ]);
     setToDo("");
     console.log(toDos);
   };
@@ -17,12 +28,12 @@ function App() {
     <div className="App">
       <h2>ToDo</h2>
       <form onSubmit={onSubmitForm}>
-        <input type="text" onChange={changeTodo} value={toDo} />
+        <input type="text" onChange={plusTodo} value={toDo} />
         <button>등록하기</button>
       </form>
       <ul>
         {toDos.map((item) => (
-          <li>{item}</li>
+          <List item={item} setToDos={setToDos} toDos={toDos}></List>
         ))}
       </ul>
     </div>
---
+++ src/components/List.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+
+const List = ({ item, toDos, setToDos, toDo }) => {
+  const changeTodo = (itemno) => {
+    const copy = [...toDos];
+    const target = copy.find((x) => x.no === itemno);
+    target.title = currentTxt;
+    setToDos(copy);
+    setIsModify(false);
+  };
+  const [isModify, setIsModify] = useState(false);
+  const [currentTxt, setCurrentTxt] = useState(item.title);
+  const onChangeTxt = (e) => {
+    setCurrentTxt(e.target.value);
+  };
+  useEffect(() => {
+    setCurrentTxt(item.title);
+  }, [isModify, item]);
+  return (
+    <li key={item.no}>
+      {isModify ? (
+        <>
+          <input type="text" value={currentTxt} onChange={onChangeTxt} />
+          <button
+            onClick={() => {
+              changeTodo(item.no);
+            }}
+          >
+            수정완료
+          </button>
+          <button
+            onClick={() => {
+              setIsModify(false);
+            }}
+          >
+            수정취소
+          </button>
+        </>
+      ) : (
+        <>
+          {item.title}
+          <button
+            onClick={() => {
+              setIsModify(true);
+            }}
+          >
+            수정
+          </button>
+          <button>삭제</button>
+        </>
+      )}
+    </li>
+  );
+};
+
+export default List;
